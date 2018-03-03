272db73d5deef8f27c0d131785ca85795f4c1902
mishana <mishakruvoruchko@gmail.com>
2018-03-03 18:23:19 +0200

    First Commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+	 var webhostBut = "\"Hosted on free web hosting 000webhost.com. Host your own website for FREE.\"";
+	 $("[title="+webhostBut+"]").parent().css("display", "none");
+$("footer *").addClass("links");
+
+
+
+
+
+});
