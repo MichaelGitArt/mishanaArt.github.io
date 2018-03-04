e89c3c7d08faadaefb4381fefe634ae688f1a217
mishana <mishakruvoruchko@gmail.com>
2018-03-04 16:45:21 +0200

    commit

--- portfolio_01/js/js.js
+++ portfolio_01/js/js.js
@@ -1,52 +1,40 @@
-var dwsSubmit = document.querySelector(".elem1 .dws-submit");
+var submit = document.querySelectorAll("input[type='submit']");
 
-var elem = document.querySelector(".elem1");
-elem.addEventListener("mouseover", function(){
-	var newElem = document.createElement("div");
-	newElem.className = "newDiv";
-	document.body.appendChild(newElem);
-	newElem.style.cssText = " \
-	position: absolute;\
-	width: 200px;\
-	height: 80px;\
-	background-color: white;\
-	border-radius: 6px;\
-	padding: 10px;\
-	font: 16px Tahoma, sans-serif;"
-	newElem.innerHTML = "Введите пожалуста свой  ник в поле ввода."
-	var coords = getCoords(elem);
-	newElem.style.top = coords.top - newElem.offsetHeight - 5 + "px";
-	newElem.style.left = coords.left + "px";
-});
+for (var i = 0; i <  submit.length; i++) {
+	submit[i].onclick= function(){return false;}
+}
+/*var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 49.54357181, lng: 25.6},
+    zoom: 16
+  });
+}*/
 
-elem.addEventListener("mouseout", function(){
-	document.body.removeChild(document.querySelector(".newDiv"));
-
-});
-
-
-dwsSubmit.addEventListener("click", pressMe);
-
-function pressMe(e){
-	var elem = document.querySelector(".elem1 .input");
-	if(elem.value === "") {
-		console.log("The strig is empty");
-		return;
-	}
-	 if(elem.value.length !== 10){
-	  alert("Надо было писать ZIP cod. Идиот!");
-	  document.querySelector(".elem1 p").innerHTML = "<a href='https://ru.wikipedia.org/wiki/ZIP-%D0%BA%D0%BE%D0%B4' target='_blank'>Что такое ZIP code</a>";
-  	dwsSubmit.removeEventListener("click", pressMe);
-	}else{
-		alert("Красавчик! Наверное...")
-	}
+function initMap() {
+  var uluru = {lat: 49.54357181, lng: 25.6};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 }
 
 
-function getCoords(elem){
-  var box = elem.getBoundingClientRect();
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset
-  };
-}
+var mapOpen = document.getElementsByClassName("map-open")[0],
+mapHide = document.getElementsByClassName("map-hide")[0],
+mapCircle = document.getElementsByClassName("map-circle")[0];
+
+//mapOpen.parentNode.style.display = "none";
+
+mapOpen.onclick = function(){
+	mapCircle.style.display = "none";
+	mapHide.style.display = "block";
+};
+mapHide.onclick = function(){
+	mapCircle.style.display = "block";
+	mapHide.style.display = "none";
+};
