a82c8cba4c2076a3f4889395c12d41efdb29808f
mishana <mishakruvoruchko@gmail.com>
2018-03-17 21:03:17 +0200

    Commit
    New mobile menu and portfolio blocks.

--- portfolio_03/js/js.js
+++ portfolio_03/js/js.js
@@ -1,32 +1,45 @@
 'use strict'
 
-// Mobile menu
-var sandwich = document.getElementById("sandwich"),
-menu = document.querySelector(".nav-menu"),
-mobMenu = document.querySelector(".mobMenu"),
-opensandwich = false;
-console.log(mobMenu);
 
-menu.onclick = function(e){
-	var target = e.target;
-	if(target.tagName === "A"){
-		toggleMenu(winWidth);
-	}
-}
-sandwich.onclick = function(){
-	sandwich.className = "active";
-	toggleMenu();
-};
+// Portfolio HOVER
+
+var works = document.querySelector(".works");
+console.log(works);
+
+
+// jQuery('.workBlock').hover(
+//     setTimeout(function () {   }, 500);
+//     setTimeout(function () { 		},);
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 //Contact
 var contactBut = document.querySelector(".contact-info .button");
 
-
-
 contactBut.addEventListener("click", function(){
-	console.log("msdkljfg");
 	var allContacts = document.createElement("div");
 
 
@@ -35,11 +48,6 @@ contactBut.addEventListener("click", function(){
 	 <br> Пишите сюда: <a href='https://telegram.me/keyper' target='_blank'>Telegram</a>\
 	 ли сюда: <a href='https://vk.com/mishana2002' target='_blank'>VK</a> ";
 	allContacts.className = "modalWin";
-
-
-
-
-
 
 
 	document.body.appendChild(allContacts);
@@ -52,41 +60,54 @@ contactBut.addEventListener("click", function(){
 
 
 
+// Mobile menu
+var sandwich = document.getElementById("sandwich"),
+menu = document.querySelector(".nav-menu"),
+mobMenu = document.querySelector("#mobMenu"),
+openedMenu = false;
 
 
-
-
-
-
-
-
-
-
-
-
+menu.onclick = function(e){
+	var target = e.target;
+	if(target.tagName === "A"){
+		toggleMenu();
+	}
+};
+mobMenu.onclick = function(e){
+	var target = e.target;
+	if(target.tagName === "A"){
+		toggleMenu();
+	}
+};
+sandwich.onclick = function(){
+	toggleMenu();
+};
 
 function toggleMenu(){
-	if(opensandwich){
-		menu.style.left = "-320px";
-		sandwich.className = "";
-		sandwich.style.right = 0;
-		opensandwich = false;
-		mobMenu.style.width = "100%"
-		mobMenu.style.left = "0px"
-		document.documentElement.style.filter = "none";
-	}else{
-		sandwich.style.width = "70px";
-		menu.style.left = "0";
-		sandwich.className = "active";
-		opensandwich = true;
-		mobMenu.style.width = "70px"
-		mobMenu.style.left = "240px"
-		document.documentElement.style.filter = "grayscale(100%)";
-	}
+	mobMenu.classList.toggle("mobMenu-active");
+	menu.classList.toggle("menu-active");
+	sandwich.classList.toggle("active");
+	return false;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function slowScroll (id) {
-	if(document.documentElement.clientWidth <= 600){
+	if(document.documentElement.clientWidth <= 550){
 		var offset = 55;
 	}else  var offset = 0;
 	$('html, body').animate ({
