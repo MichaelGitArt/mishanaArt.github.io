d3109856e1d1c83f66b779b604d9a06d390037d1
mishana <mishakruvoruchko@gmail.com>
2018-04-29 22:16:28 +0300

    Mobile

--- portfolio_05/js/js.js
+++ portfolio_05/js/js.js
@@ -31,7 +31,6 @@ document.body.onload = function(){
 
 		// выезжающие изображения
 		let goingBlock = document.querySelectorAll('.going');
-			console.log(goingBlock);
 				setTimeout(function(){
 					for (var i = 0; i < goingBlock.length; i++) {
 						goingBlock[i].classList.add('show');
@@ -70,17 +69,26 @@ document.body.onload = function(){
 	},1000)
 }
 
-// Адаптация 
+///////////////////////////////////////////////////
+////////////////  Адаптация ///////////////////////
+///////////////////////////////////////////////////
+
 	let wrapper = document.getElementsByClassName('wrapper')[0];
 	let backButton = document.getElementsByClassName('back')[0];
 	let forwardButton = document.getElementsByClassName('forward')[0];
 
 
+	let screenSize = '1000+';
+
+// Начальное мещение wrapper;
+	let xMixing = 0;
+
+// Обработчик для сдвига разноцветных блоков
 	document.addEventListener('click', function(event){
 		let target = event.target;
 		while(target != document.body){
 				if(target.className == 'back' || target.className == 'forward' ){
-					moveBlock(target.className);
+					moveBlock(target.className, screenSize);
 					return;
 				}
 			target = target.parentNode;
@@ -89,39 +97,100 @@ document.body.onload = function(){
 
 
 
-	let screenSize;
 
-	if(matchMedia){
-		let screen = window.matchMedia('(max-width: 1000px)');
-		screen.addListener(changes);
-		changes(screen);
+	//// Обработчики для экранов разной ширины ////
+
+	// вспомогательная функция для проверки ширины экрана
+	let testWidth = 'first';
+
+
+// Проверка  меньше или больше 700px
+	if(true){
+		let screen = window.matchMedia('(max-width: 700px)');
+		screen.addListener(changeScreen2);
+		changeScreen2(screen);
 	}
-	function changes(screen){
+// Проверка больше чем 1000px
+	if(true){
+		let screen = window.matchMedia('(max-width: 1000px)');
+		screen.addListener(changeScreen1);
+		changeScreen1(screen);
+	}
+
+
+
+// Запустить если экран 0-1000px
+	function changeScreen2(screen){
+		// 0-700
 		if(screen.matches){
-			console.log('700+');
-			screenSize = '700+'
+			xMixing = 0
+			screenSize = '700-';
+			if(testWidth === 'first'){
+			testWidth = true;
+			}
+			console.log('0-700');
 			wrapper.style.transform = 'translate(0%, 0)'
 			forwardButton.classList.remove('hide');
 			backButton.classList.add('hide');
+		// 700-1000
 		}else{
-			console.log('1000+');
-			screenSize = '1000+'
-			// wrapper.style.transform = 'translate(0%, 0)'
+			wrapper.style.transform = 'translate(0%, 0)'
+			xMixing = 0
+			screenSize = '1000-';
+		}
+	}
+
+// Запустить если экран 1000+px
+	function changeScreen1(screen){
+		if(!screen.matches){
+			wrapper.style.transform = 'translate(0%, 0)'
 		}
 	}
 
 
-function moveBlock(value){
-	if (value === "back"){
-		wrapper.style.transform = 'translate(0%, 0)'
-			forwardButton.classList.remove('hide');
-			backButton.classList.add('hide');
-	}else{
-		wrapper.style.transform = 'translate(-50%, 0)'
-			forwardButton.classList.add('hide');
-			backButton.classList.remove('hide');
-	}
 
+
+
+
+function moveBlock(value, screenSize){
+	// Коофициент сдвига
+	let k = 1;
+	if(screenSize === "700-") k = 2;
+
+
+	// Значение сдвига с учетом коофициента
+	if (value === "back"){
+		xMixing += 50 * k;
+	}else{
+		xMixing -= 50 * k;
+	}
+	// Движем
+	wrapper.style.transform = `translate(${xMixing}%, 0)`
+
+
+	//Убираем и показываем кнопки для экранов 700-1000
+		if(+k === 1 && +xMixing === -50) {
+			forwardButton.classList.add('hide')
+		}else{
+			forwardButton.classList.remove('hide')
+		}
+		if(+k === 1 && +xMixing === 0) {
+			backButton.classList.add('hide')
+		}else{
+			backButton.classList.remove('hide')
+		}
+	//Убираем и показываем кнопки для экранов 0-700
+
+	if(+k === 2 && +xMixing === -200) {
+			forwardButton.classList.add('hide')
+		}else{
+			forwardButton.classList.remove('hide')
+		}
+		if(+k === 2 && +xMixing === 0) {
+			backButton.classList.add('hide')
+		}else{
+			backButton.classList.remove('hide')
+		}
 }
 
 
