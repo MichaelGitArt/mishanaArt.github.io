8271e19c5c25d2456d5bcf6b50788d3fe07634e2
mishana <mishakruvoruchko@gmail.com>
2018-07-07 21:36:53 +0300

    Commit

--- sites/test/js/common.js
+++ sites/test/js/common.js
@@ -1,142 +1,91 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 46.474, lng: 30.731},
-    zoom: 16
-  });
-}
-
 $(function() {
 
-	$(".reviews-container").on('click', '.reviews-item', function(){
-		let newSrc = $(this).find('.reviews-img').attr('src').replace('.jpg', '-big.jpg')
-		$('.zoom-modal').addClass('on').find('img').attr('src', newSrc);
-	});
-
-	$('.zoom-modal').click(function(e){
-		if(e.target.classList.contains('zoom-modal')){
-			$(this).removeClass('on')
-		}
-	})
-	$('body').on('click', '.close', function(){
-		$(this).parent().parent().removeClass('on')
-		$(this).parent().removeClass('on')
+	$('#toggle').click(function(){
+		$(this).toggleClass('on');
+		$('.menu ul').slideToggle();
 	})
 
-
-$('.staff .team-btn').click(function(){
-	let element = $(this).closest('.staff-item').clone();
-	console.log(element);
-	$('.team-modal').empty().append(element).addClass('on')
-	$('.close').clone().appendTo('.team-modal .staff-item').addClass('close_circle');
-	return false;
-})
-
-	$('.team-modal').click(function(e){
-		if(e.target.classList.contains('team-modal')){
-			$(this).removeClass('on')
-		}
-	})
-
-	$("body").on('click', '.crt-item', function(){
-		console.log($(this));
-		let newSrc = $(this).find('img').attr('src').replace('.jpg', '-big.jpg')
-		$('.zoom-modal').addClass('on').find('img').attr('src', newSrc);
-	});
-
-// Кнопка записаться
-$(".btn-form").on('click', function(){
-		$('.signUp-modal-wrap').addClass('on');
-	});
-
-	$('.signUp-modal-wrap').click(function(e){
-		if(e.target.classList.contains('signUp-modal-wrap')){
-			$(this).removeClass('on')
-		}
-	})
-
-
-// Для акодиона
-
-$('.accordion').on('click', '.accordion-item', function(e){
-	if(e.target.classList.contains('accordion-arrow') || e.target.classList.contains('accordion-item'))
-	$(this).toggleClass('on');
-})
-
-// навигация по странице
-
-$('.pr-nav').on('click', 'a', function(){
-	let scroll_el = $(this).attr('href'); 
-      if ($(scroll_el).length != 0) { 
-    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+	$('.tabs-box_1').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    swipe: false,
+    arrows: true,
+    spead: 1500,
+    dots: true,
+    customPaging : function(slider, i) {
+      let thumb = $(slider.$slides[i]).data('thumb');
+      return '';
+    },
+    prevArrow: '<div class="slick-arrow slick-arrow-prev"><div class="arrow-form"></div></i></div>',
+    nextArrow: '<div class="slick-arrow slick-arrow-next"><div class="arrow-form"></div></div>',
+    responsive: [
+    {
+      breakpoint: 991, 
+      settings: {
+        slidesToShow: 3,
       }
+    },
+    {
+      breakpoint: 767, 
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480, 
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+    ]
+  });
+
+  $('.pr-tabs').on('click', '.pr-tab a', function(){
+    let index = $(this).attr('href').substring(5);
+    $('.pr-tab').removeClass('active');
+    $(this).parent().addClass('active');
+
+    $('.tabs-box').addClass('hidden');
+    $('.tabs-box_' + index).removeClass('hidden');
+
+    if(! $('.tabs-box_' + index).hasClass('slick-initialized')){
+      $('.tabs-box_' + index).slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        swipe: false,
+        arrows: true,
+        spead: 1500,
+        dots: true,
+        customPaging : function(slider, i) {
+          let thumb = $(slider.$slides[i]).data('thumb');
+          return '';
+        },
+        prevArrow: '<div class="slick-arrow slick-arrow-prev"><div class="arrow-form"></div></i></div>',
+        nextArrow: '<div class="slick-arrow slick-arrow-next"><div class="arrow-form"></div></div>',
+        responsive: [
+        {
+          breakpoint: 991, 
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 767, 
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 480, 
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+        ]
+      });
+    }
     return false;
-});
-
-$('.pr-button-more button').click(function(){
-	$('.pr-example-hidden').slideToggle(800);
-	$(this).children('span').toggleClass('on')
-});
-
-
-
-
-
-
-// Для табов
-
-$('.reasons-tabs-wrap').on('click', '.reasons-btn', function(){
-	let index = $('.reasons-btn').index(this);
-
-	$('.reasons-btn').removeClass('on');
-	$(this).addClass('on')
-
-
-	$('.reasons-tabs').removeClass('on');
-	$('.reasons-tabs:eq(' + index + ')').addClass('on');
-
-});
-
-// Цены
-$('.price').on('click', '.price-item', function(){
-	$(this).parent().toggleClass('on').children('.price-sub').slideToggle("normal", 'linear');
-});
-
-
-$('.reviews-container').slick({
-  spead: 1500,
-   slidesToShow: 5,
-  slidesToScroll: 1,
-  prevArrow: '<button type="button" class="slick-arrow slick-arrow-prev"><i class="far fa-angle-left"></i></button>',
-  nextArrow: '<button type="button" class="slick-arrow slick-arrow-next"><i class="far fa-angle-right"></i></button>'
- });
-
-$('.main_staff').slick({
-  spead: 1500,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: '<button type="button" class="slick-arrow slick-arrow-prev"><i class="far fa-angle-left"></i></button>',
-  nextArrow: '<button type="button" class="slick-arrow slick-arrow-next"><i class="far fa-angle-right"></i></button>'
- });
-
-
-$('.main-head').slick({
-	 slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-	spead: 1500,
-  dots: true,
-  customPaging : function(slider, i) {
-    let thumb = $(slider.$slides[i]).data('thumb');
-    return '';
-  }
-})
-
-
-
-
-
-
+  });
 
 
 });
