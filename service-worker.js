a94e36424367fa216c98a05ab5a1089e592c1eb4
mishana <34610289+mishanaArt@users.noreply.github.com>
2019-05-18 12:05:08 +0300

    Commit

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,32 @@
-self.addEventListener('fetch', function(event){
+var cacheAllowed = /.=\.(js|css|png|jpg|jpeg|php|ico|json)$/;
+self.addEventListener('fetch', function (event) {
 	// console.log(event.request.url);
+	// if (event.request.method === 'GET' && cacheAllowed.test(event.request.url)) {
+	event.respondWith(
+		caches.match(event.request.url)
+			.then(response => {
+				if (response) {
+					// console.warn('response', response)
+					return response;
+				} else {
+					console.warn('no response!')
+					console.warn('event.request.url', event.request.url)
+
+					return fetch(event.request)
+						.then(res => {
+							return caches.open('cache-version-1')
+								.then(cache => {
+									console.log('Catch!')
+									cache.put(event.request.url, res.clone());
+									return res;
+								})
+
+						})
+						.catch(err => {
+							console.log('silense!')
+						})
+				}
+			})
+	);
+	// }
 })
